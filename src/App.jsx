e0dc160f6e436b078063805d7b55187ab6775296
M.Usama <usamazarishah@gmail.com>
2024-10-22 22:45:54 +0500

    React_Project_2_done

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="intro">
+      <h1>Welcome to my website!</h1>
+    
+    <p className="summary">
+      You can find my thoughts here.
+      <br></br>
+      <b>And </b><i>pictures</i> of scientists!
+    </p>
+    </div>
+  );
+}
+
+export default App
